34e28b7e12f0fdf8eb6b05f92d2125157186c1a4
root <root@BE3>
2022-06-22 21:40:56 +0000

    pinia socket plugin

--- myfirstvue/vite.config.js
+++ myfirstvue/vite.config.js
@@ -25,17 +25,11 @@ export default defineConfig({
             changeOrigin: true,
             secure: false
 
-<<<<<<< HEAD
-            },'/socket.io': {
-                target: 'http://localhost:89',
-                ws: false
-
-=======
             },
             '/socket.io': {
                 target: 'http://localhost:89',
                 ws: false
->>>>>>> 3658b2a8db4aa610b1d2303ad3cc68ebdc5dcb6c
+
             }
 
         },
